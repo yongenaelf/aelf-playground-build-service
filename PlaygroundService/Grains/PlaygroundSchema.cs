9efa17be68aa93b3bb75f48c1c1222f93586b9af
lkanthp <lakshmi.kanth@aelf.io>
2024-05-29 07:44:51 +0530

    chore: remove obselete structs

--- PlaygroundService/Grains/PlaygroundSchema.cs
+++ PlaygroundService/Grains/PlaygroundSchema.cs
@@ -4,31 +4,6 @@ namespace PlaygroundService.Grains;
 
 public abstract class PlaygroundSchema
 {
-    public class PlaygroundContractGenerateRequest
-    {
-        
-        [JsonPropertyName("ProjectName")]
-        public string ProjectName { get; set; }
-        
-        [JsonPropertyName("ContractClass")]
-        public string contractClass { get; set; }
-        
-        [JsonPropertyName("ContractClassName")]
-        public string contractClassName { get; set; }
-        
-        [JsonPropertyName("StateClass")]
-        public string stateClass { get; set; }
-        
-        [JsonPropertyName("StateClassName")]
-        public string stateClassName { get; set; }
-        
-        [JsonPropertyName("Proto")]
-        public string proto { get; set; }
-        
-        [JsonPropertyName("ProtoFilename")]
-        public string protoFilename { get; set; }
-    }
-    
     public class PlaygroundContractGenerateResponse
     {
         [JsonPropertyName("success")]
